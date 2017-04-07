8a02b5e142e6e930ff2a2dd8f8e274a45898ea74
gp22 <gp22@tutanota.com>
2017-04-07 23:43:11 +0700

    add filtering of Yelp responses

--- server/routes/query.js
+++ server/routes/query.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const _ = require('lodash');
 
 const YELP_API_KEY = process.env.YELP_API_KEY;
 const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=coffee&location=';
@@ -12,13 +13,21 @@ router.get('/api/:city', (req, res) => {
     }
     const encodedCity = encodeURIComponent(req.params.city);
     const url = yelpUrl + encodedCity;
+    const businessList = [];
+
     axios.get(url, {
         headers: {
-            'Authorization': 'Bearer ' + YELP_API_KEY
+            Authorization: 'Bearer ' + YELP_API_KEY
         }
     }).then((response) => {
-        res.send(response.data);
-        res.send();
+        response.data.businesses.forEach((element) => {
+            const business = _.pick(element, ['name', 'url', 'location.address1']);
+            businessList.push(business);
+            // console.log(business);
+        });
+        res.send(businessList);
+        // res.send(response.data);
+        // res.send();
     }).catch((e) => {
         res.status(400).send();
     });
