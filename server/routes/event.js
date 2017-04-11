73154bcf1fd40133f059659a502786064de48d94
gp22 <gp22@tutanota.com>
2017-04-11 11:25:23 +0700

    add ability to add users to events, and work on query route

--- server/routes/event.js
+++ server/routes/event.js
@@ -9,18 +9,23 @@ const _ = require('lodash');
 
 router.use(bodyParser.json());
 
-// SIGNUP route
-router.post('/event/:userId', (req, res) => {
+// Route to add user to event
+router.post('/:eventId/:userId', (req, res) => {
   // get eventId and userId from query
-  const eventData = _.pick(req.body, ['venue']);
-  eventData.going = req.params.userId;
+  userId = req.params.userId;
+  eventId = req.params.eventId;
+  // const eventData = _.pick(req.body, ['venue']);
+  // eventData.going = userId;
 
   // query db to see if there is already an event with the same eventId
-  Event.findOne({ venue: eventData.venue }).then((event) => {
+  Event.findOne({ venue: eventId }).then((event) => {
     // if there isn't:
     if (!event) {
       // create the new event
-      const event = new Event(eventData);
+      const event = new Event({
+        venue: eventId,
+        going: userId
+      });
       event.save().then(() => {
         return res.send({ event });
       });
@@ -28,9 +33,20 @@ router.post('/event/:userId', (req, res) => {
 
     // if there is:
     // check to see if the user is already going:
-    // if they are: remove them
-    // if they aren't: add them    
-    res.send({ event });
+    if (event.going.indexOf(req.params.userId) !== -1) {
+      // if they are: remove them
+      index = event.going.indexOf(userId);
+      event.going.splice(index, 1);
+      event.save().then(() => {
+        return res.send({ event });
+      });
+    } else {
+      // if they aren't: add them
+      event.going.push(userId);
+      event.save().then(() => {
+        return res.send({ event });
+      });
+    }
   }).catch((e) => {
     res.status(400).send();
   });
--- server/routes/query.js
+++ server/routes/query.js
@@ -1,41 +1,53 @@
 const express = require('express');
 const router = express.Router();
+
 const axios = require('axios');
+
+const mongoose = require('mongoose');
 const Event = require('./../models/event');
-// const _ = require('lodash');
 
 const YELP_API_KEY = process.env.YELP_API_KEY;
 const yelpUrl = 'https://api.yelp.com/v3/businesses/search?term=coffee&location=';
 
 // QUERY Route
 router.get('/api/:city', (req, res) => {
-    if (req.params.city === "''") {
-        return res.status(411).send();
-    }
-    const encodedCity = encodeURIComponent(req.params.city);
-    const url = yelpUrl + encodedCity;
-    // The filtered list of businesses to send back to the client
-    // after the Yelp query.
-    const businessList = [];
+  if (req.params.city === "''") {
+    return res.status(411).send();
+  }
+  const encodedCity = encodeURIComponent(req.params.city);
+  const url = yelpUrl + encodedCity;
+  // The filtered list of businesses to send back to the client
+  // after the Yelp query.
+  const businessList = [];
 
-    axios.get(url, {
-        headers: {
-            Authorization: 'Bearer ' + YELP_API_KEY
+  axios.get(url, {
+    headers: {
+      'Authorization': 'Bearer ' + YELP_API_KEY
+    }
+  }).then((response) => {
+    response.data.businesses.forEach((business) => {
+      // Search the db for events with a venue matching the id
+      Event.findOne({ event: business.id }).then((event) => {
+        let businessToPush = {
+          name: business.name,
+          url: business.url,
+          id: business.id,
+          location: business.location.address1
+        };
+        // if no event is found, set the number of people going to 0
+        if (!event) {
+          businessToPush.going = 0;
+        } else {
+        // if there is one, add the number of people going to the business
+          businessToPush.going = event.going.length;
         }
-    }).then((response) => {
-        response.data.businesses.forEach((business) => {
-            // Search the db for events with a venue matching the id
-            let businessToPush = {};
-            businessToPush.name = business.name;
-            businessToPush.url = business.url;
-            businessToPush.id = business.id;
-            businessToPush.location = business.location.address1;
-            businessList.push(businessToPush);
-        });
-        res.send(businessList);
-    }).catch((e) => {
-        res.status(400).send();
+        businessList.push(businessToPush);
+      });
     });
+    res.send(businessList);
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 module.exports = router;
