915d80dc49d868b3362bb9b3f1c319b37e1083dd
gp22 <gp22@tutanota.com>
2017-04-16 11:27:10 +0700

    update comments

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 /*
-This file sets up defaults for the environment including the Yelp API key, and
-variables that depend on whether the app is running locally or on Heroku.
+This file sets up defaults for the environment, and variables that depend on
+whether the app is running locally or on Heroku.
 If it's running on Heroku then the environment variable process.env.NODE_ENV
-will exist and the port and MONGODB_URI will be set. Otherwise, if it's running
-locally we set the port and MONGODB_URI depending on if we're testing or not.
+will exist and PORT and MONGODB_URI will be set automatically by Heroku.
+Otherwise, if it's running locally we set PORT and MONGODB_URI depending on
+if we're testing or not.
 */
 const env = process.env.NODE_ENV || 'development';
 
