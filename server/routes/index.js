af4844c442158ff0cb1e40a95d4996bbd40133d4
gp22 <gp22@tutanota.com>
2017-05-16 11:57:50 +0700

    add index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 // HOME route
 router.get('/', (req, res) => {
-  res.send('coffeeTime!');
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
 
 module.exports = router;
